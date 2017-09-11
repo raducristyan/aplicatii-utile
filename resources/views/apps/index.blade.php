90c96592f594b0a4ecc0a7d1a8fb41a620df790f
Radu Crisrian Burduhos <raducristyan@gmail.com>
2017-09-11 21:08:30 +0300

    Push to heroku

--- resources/views/apps/index.blade.php
+++ resources/views/apps/index.blade.php
@@ -4,7 +4,7 @@
     <div class="col-12">
         <div class="card">
             <div class="card-block">
-                Listă aplicații
+                Listă aplicații disponibile
             </div>
         </div>
     </div>
