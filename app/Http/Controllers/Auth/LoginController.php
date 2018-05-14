a92bb8a87a9f680c8a52e50a837cfd1ea8cda7d1
Radu Cristian BURDUHOS <raducristyan@gmail.com>
2018-05-14 22:27:55 +0300

    Install yarn dependecies

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -53,7 +53,7 @@ class LoginController extends Controller
             Auth::logout($user);
             
             if ($user->is_admin) {
-                flash('Vă rugăm să activați contul instituției. <a href="' . route('activate.resend') . '?email=' . $user->email . '">Retransmite email-ul pentru activare.</a>');
+                flash('Vă rugăm să activați contul instituției. <a href="' . route('activate.resend') . '?email=' . $user->email . '">Retransmite email-ul pentru activare.</a>')->important();
                 return redirect('/login');
             } else {
                 flash('Contul instituției nu a fost activat. Contactați administratorul contului');
