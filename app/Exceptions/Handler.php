b81f18843ea8f41d10d83d5131670064b28d6f83
Radu Cristian BURDUHOS <raducristyan@gmail.com>
2019-02-19 23:18:38 +0200

    Update env check in Exception Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -71,7 +71,7 @@ class Handler extends ExceptionHandler
      */
     protected function convertExceptionToResponse(Exception $e)
     {
-        if (config('app.debug') && $this->app->environment('local', 'testing') ) {
+        if (config('app.debug') && (config('app.env') = 'local' || config('app.env') = 'testing') ) {
             $whoops = new \Whoops\Run;
             $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 
