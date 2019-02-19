21bc9fabb70685163ed26c6bf7d67113434ee222
Radu Cristian BURDUHOS <raducristyan@gmail.com>
2019-02-19 20:01:58 +0200

    Deleted optimise command - composer.json

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,11 @@
+
+<script>
+    import example from './components/Example.vue'
+    
+    export default {
+        name: 'app',
+        components: {
+            example
+        }
+    }
+</script>
---
+++ resources/views/admin/layout.blade.php
@@ -0,0 +1,1 @@
+@extends('apps.layout')
